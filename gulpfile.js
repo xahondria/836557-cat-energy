e6f9e65120df1843437d8c575846e53abd9386cc
Vladimir Kolomoyets <kolomoyetsva@gmail.com>
2018-10-25 18:03:14 +0300

    добавлены webp версии изображений

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var server = require("browser-sync").create();
 var csso = require("gulp-csso");
 var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -33,6 +34,12 @@ gulp.task("images", function () {
     ]))
     .pipe(gulp.dest("source/img"));
 });
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"));
+})
 
 gulp.task("copy", function () {
   return gulp.src([
